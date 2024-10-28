4dfa2e4d6dae9050829315831bccf2a33dd344d3
VIE <anq.viet203@gmail.com>
2024-10-28 18:24:44 +0700

    initialize project

---
+++ src/main/java/fit/se/vttblog/VttBlogApplication.java
@@ -0,0 +1,12 @@
+package fit.se.vttblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttBlogApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(VttBlogApplication.class, args);
+   }
+}
---
+++ src/test/java/fit/se/vttblog/VttBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.se.vttblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
